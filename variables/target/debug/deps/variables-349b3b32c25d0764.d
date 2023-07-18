03474c7e442987181444f8de2a28c9097c772880
Guillaume Marc <guilmarc@mnt.fr>
2023-07-18 08:30:42 -0400

    variables lessons

---
+++ variables/target/debug/deps/variables-349b3b32c25d0764.d
@@ -0,0 +1,5 @@
+/home/vitus/Projets/rust-book/variables/target/debug/deps/variables-349b3b32c25d0764: src/main.rs
+
+/home/vitus/Projets/rust-book/variables/target/debug/deps/variables-349b3b32c25d0764.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/vitus/Projets/rust-book/variables/target/debug/variables: /home/vitus/Projets/rust-book/variables/src/main.rs
